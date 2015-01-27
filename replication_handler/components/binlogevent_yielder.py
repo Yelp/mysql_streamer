7d8b85bfecf09a1039d1d91bf313e7cff856bd48
Ryan Irwin <ryani@yelp.com>
2015-01-27 10:02:17 -0800

    committing a very light wrapper around the replication stream

---
+++ replication_handler/components/binlogevent_yielder.py
@@ -0,0 +1,21 @@
+from pymysqlreplication import BinLogStreamReader
+
+class BinlogEventYielder(object):
+
+    def __init__(self, repl_mysql_settings):
+        """These are our default settings.
+           server_id doesn't seem to matter but must be set.
+           blocking will keep this iterator infinite
+        """
+        self.stream = BinLogStreamReader(
+            connection_settings=repl_mysql_settings,
+            server_id=1,
+            blocking=True,
+            only_events=[RotateEvent, QueryEvent, WriteRowsEvent]
+        )
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.stream.fetchone()
---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='replication_handler',
+    version='0.1.0',
+    description='',
+    author='BAM',
+    author_email='bam@yelp.com',
+    url='https://gitweb.yelpcorp.com/?p=replication_handler.git',
+    packages=find_packages(exclude=['tests']),
+    setup_requires=['setuptools'],
+    install_requires=[
+        'mysql-replication',
+    ],
+    license='Copyright Yelp 2015, All Rights Reserved'
+)
