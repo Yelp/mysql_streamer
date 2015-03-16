697d91d425161ef77fe756d4951189278363cc11
Cheng Chen <cheng@yelp.com>
2015-03-16 16:23:21 -0700

    add column

--- models/event_state.py
+++ models/event_state.py
@@ -1,18 +1,13 @@
 # -*- coding: utf-8 -*-
 from sqlalchemy import Column
 from sqlalchemy import Integer
+from sqlalchemy import String
 from sqlalchemy import Text
 from sqlalchemy.types import Enum
 
 from models.database import Base
 from models.database import UnixTimeStampType
 from models.database import default_now
-
-
-event_status = [
-    'Pending',
-    'Completed'
-]
 
 
 class EventStatus(object):
@@ -26,8 +21,9 @@ class EventState(Base):
     __tablename__ = 'event_state'
 
     id = Column(Integer, primary_key=True)
-    # Text type because gtid can be pretty lengthy.
-    gtid = Column(Text, nullable=False)
+    gtid = Column(String, nullable=False)
+    # Data event could have multiple rows.
+    offset = Column(Integer, nullable=False)
     status = Column(
         Enum(
             EventStatus.PENDING,
@@ -37,9 +33,14 @@ class EventState(Base):
         default=EventStatus.PENDING,
         nullable=False
     )
-    # The query that needs to be executed and register with Schematizer.
+    # The query that needs to be processed:
+    #   - If this query is a DDL statement, we execute in local schema trackering db and
+    #   register with Schematizer.
+    #   - If this query is a DML statement, we schematize the payload and publish to kafka.
     query = Column(Text, nullable=False)
-    # Snapshot before executing the incoming query.
+    # Useful for partition by table_name later.
+    table_name = Column(String, nullable=False)
+    # Table snapshot before executing the incoming query.
     create_table_statement = Column(Text, nullable=False)
     time_created = Column(UnixTimeStampType, default=default_now)
     time_updated = Column(UnixTimeStampType, default=default_now, onupdate=default_now)
--- schema/tables/state.sql
+++ schema/tables/state.sql
@@ -1,10 +1,13 @@
 CREATE TABLE `event_state` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `gtid` text NOT NULL,
+  `gtid` varchar(255) NOT NULL,
+  `offset` int(11) NOT NULL,
   `status` varchar(20) NOT NULL DEFAULT 'Pending',
   `query` text NOT NULL,
+  `table_name` varchar(255) NOT NULL,
   `create_table_statement` text NOT NULL,
   `time_created` int(11) NOT NULL,
   `time_updated` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `gtid` (`gtid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
