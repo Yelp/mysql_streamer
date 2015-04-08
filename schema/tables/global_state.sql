7f7dc1a9d917a7a7812ca287e3e0dbca740d56c2
Cheng Chen <cheng@yelp.com>
2015-04-08 11:48:14 -0700

    fix syntax

--- schema/tables/global_state.sql
+++ schema/tables/global_state.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `global_state` (
   `gtid` varchar(255) NOT NULL,
   `is_clean_shutdown` tinyint(1) NOT NULL,
-  PRIMARY KEY (`gtid`),
+  PRIMARY KEY (`gtid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
