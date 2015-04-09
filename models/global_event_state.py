2a79b54ad2e9deb88dc81303563e84db780b27b4
Cheng Chen <cheng@yelp.com>
2015-04-09 13:38:44 -0700

    add data event so we know which table to look at

--- models/global_event_state.py
+++ models/global_event_state.py
@@ -2,10 +2,17 @@
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import String
+from sqlalchemy.types import Enum
 
 from models.database import Base
 from models.database import UnixTimeStampType
 from models.database import default_now
+
+
+class EventType(object):
+
+    SCHEMA_EVENT = 'schema_event'
+    DATA_EVENT = 'data_event'
 
 
 class GlobalEventState(Base):
@@ -14,4 +21,12 @@ class GlobalEventState(Base):
 
     gtid = Column(String, primary_key=True)
     is_clean_shutdown = Column(Integer, nullable=False)
+    event_type = Column(
+        Enum(
+            EventType.SCHEMA_EVENT,
+            EventType.DATA_EVENT,
+            name='event_type'
+        ),
+        nullable=False
+    )
     time_updated = Column(UnixTimeStampType, default=default_now, onupdate=default_now)
--- schema/tables/global_event_state.sql
+++ schema/tables/global_event_state.sql
@@ -1,6 +1,7 @@
-CREATE TABLE `global_state` (
+CREATE TABLE `global_event_state` (
   `gtid` varchar(255) NOT NULL,
   `is_clean_shutdown` tinyint(1) DEFAULT 0 NOT NULL,
+  `event_type` varchar(20) NOT NULL,
   `time_updated` int(11) NOT NULL,
   PRIMARY KEY (`gtid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
