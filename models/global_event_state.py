e688ed1593ce1273eeec784da19738c99a1519d8
Cheng Chen <cheng@yelp.com>
2015-04-08 19:10:19 -0700

    add time_updated

--- models/global_event_state.py
+++ models/global_event_state.py
@@ -4,6 +4,8 @@ from sqlalchemy import Integer
 from sqlalchemy import String
 
 from models.database import Base
+from models.database import UnixTimeStampType
+from models.database import default_now
 
 
 class GlobalEventState(Base):
@@ -12,3 +14,4 @@ class GlobalEventState(Base):
 
     gtid = Column(String, primary_key=True)
     is_clean_shutdown = Column(Integer, nullable=False)
+    time_updated = Column(UnixTimeStampType, default=default_now, onupdate=default_now)
--- schema/tables/global_event_state.sql
+++ schema/tables/global_event_state.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `global_state` (
   `gtid` varchar(255) NOT NULL,
   `is_clean_shutdown` tinyint(1) DEFAULT 0 NOT NULL,
+  `time_updated` int(11) NOT NULL,
   PRIMARY KEY (`gtid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
