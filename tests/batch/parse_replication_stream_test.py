2268a5d96d6fb8add627b16bf3a647fdf7751a47
Cheng Chen <cheng@yelp.com>
2015-04-13 16:29:56 -0700

    fix test

--- tests/batch/parse_replication_stream_test.py
+++ tests/batch/parse_replication_stream_test.py
@@ -6,7 +6,6 @@ from pymysqlreplication.event import QueryEvent
 from pymysqlreplication.row_event import WriteRowsEvent
 
 from replication_handler.batch.parse_replication_stream import ParseReplicationStream
-from replication_handler.components.binlogevent_yielder import BinlogEventYielder
 from replication_handler.components.binlogevent_yielder import ReplicationHandlerEvent
 from replication_handler.components.data_event_handler import DataEventHandler
 from replication_handler.components.schema_event_handler import SchemaEventHandler
@@ -28,10 +27,9 @@ class TestParseReplicationStream(object):
         schema_event,
         data_event,
     ):
-        with mock.patch.object(
-            BinlogEventYielder,
-            'next'
-        ) as mock_yielder_next:
+        with mock.patch(
+            'replication_handler.batch.parse_replication_stream.BinlogEventYielder'
+        ) as mock_binlog_yielder:
             schema_event_with_gtid = ReplicationHandlerEvent(
                 gtid="fake_gtid_1",
                 event=schema_event
@@ -40,11 +38,11 @@ class TestParseReplicationStream(object):
                 gtid="fake_gtid_2",
                 event=data_event
             )
-            mock_yielder_next.side_effect = [
+            mock_binlog_yielder.return_value.__iter__.return_value = [
                 schema_event_with_gtid,
                 data_event_with_gtid
             ]
-            yield mock_yielder_next
+            yield mock_binlog_yielder
 
     @pytest.yield_fixture
     def patch_data_handle_event(self):
