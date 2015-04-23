06bb1422bfb5e3124528e4fe9f484199bcff8ccd
Cheng Chen <cheng@yelp.com>
2015-04-22 17:58:57 -0700

    mark clean shutdown for  data

--- replication_handler/batch/parse_replication_stream.py
+++ replication_handler/batch/parse_replication_stream.py
@@ -1,13 +1,27 @@
 # -*- coding: utf-8 -*-
 from collections import defaultdict
+from collections import namedtuple
+import logging
+import signal
+import sys
+
 from pymysqlreplication.event import QueryEvent
 from pymysqlreplication.row_event import UpdateRowsEvent
 from pymysqlreplication.row_event import WriteRowsEvent
 
 from yelp_batch import Batch
+from replication_handler.components.binlogevent_yielder import BinlogEventYielder
 from replication_handler.components.data_event_handler import DataEventHandler
 from replication_handler.components.schema_event_handler import SchemaEventHandler
-from replication_handler.components.binlogevent_yielder import BinlogEventYielder
+from replication_handler.components.stubs.stub_dp_clientlib import DPClientlib
+from replication_handler.models.database import rbr_state_session
+from replication_handler.models.data_event_checkpoint import DataEventCheckpoint
+from replication_handler.models.global_event_state import EventType
+from replication_handler.models.global_event_state import GlobalEventState
+
+log = logging.getLogger('replication_handler.batch.parse_replication_stream')
+
+HandlerInfo = namedtuple("HandlerInfo", ("event_type", "handler"))
 
 
 class ParseReplicationStream(Batch):
@@ -21,26 +35,63 @@ class ParseReplicationStream(Batch):
            that will encapsulate payloads.
     """
     notify_emails = ['bam+batch@yelp.com']
+    current_event_type = None
 
     def __init__(self):
         super(ParseReplicationStream, self).__init__()
 
-    def run(self):
-
+        self.dp_client = DPClientlib()
+        self.binlog_event_yielder = BinlogEventYielder()
         data_event_handler = DataEventHandler()
         schema_event_handler = SchemaEventHandler()
-        binlog_event_yielder = BinlogEventYielder()
 
-        handler_map = defaultdict()
-        handler_map[WriteRowsEvent] = data_event_handler
-        handler_map[UpdateRowsEvent] = data_event_handler
-        handler_map[QueryEvent] = schema_event_handler
+        self.handler_map = defaultdict()
+        self.handler_map[WriteRowsEvent] = HandlerInfo(
+            event_type=EventType.DATA_EVENT,
+            handler=data_event_handler
+        )
+        self.handler_map[UpdateRowsEvent] = HandlerInfo(
+            event_type=EventType.DATA_EVENT,
+            handler=data_event_handler
+        )
+        self.handler_map[QueryEvent] = HandlerInfo(
+            event_type=EventType.SCHEMA_EVENT,
+            handler=schema_event_handler
+        )
 
-        for replication_handler_event in binlog_event_yielder:
-            handler_map[replication_handler_event.event.__class__].handle_event(
+        # Register the handler for SIGINT(KeyboardInterrupt) and SigTerm
+        signal.signal(signal.SIGINT, self._handle_graceful_termination)
+        signal.signal(signal.SIGTERM, self._handle_graceful_termination)
+
+    def run(self):
+        for replication_handler_event in self.binlog_event_yielder:
+            event_class = replication_handler_event.event.__class__
+            self.current_event_type = self.handler_map[event_class]
+            self.handler_map[event_class].handler.handle_event(
                 replication_handler_event.event,
                 replication_handler_event.gtid
             )
+
+    def _handle_graceful_termination(self, signal, frame):
+        # We will perform recovery for SchemaEvent anyway.
+        if self.current_event_type == EventType.DATA_EVENT:
+            self.dp_client.flush()
+            offset_info = self.dp_client.get_latest_published_offset()
+            with rbr_state_session.connect_begin(ro=False) as session:
+                DataEventCheckpoint.create_data_event_checkpoint(
+                    session=session,
+                    gtid=offset_info.gtid,
+                    offset=offset_info.offset,
+                    table_name=offset_info.table_name
+                )
+                GlobalEventState.upsert(
+                    session=session,
+                    gtid=offset_info.gtid,
+                    event_type=EventType.DATA_EVENT,
+                    is_clean_shutdown=True
+                )
+        log.info("Gracefully shutting down.")
+        sys.exit()
 
 
 if __name__ == '__main__':
--- tests/batch/parse_replication_stream_test.py
+++ tests/batch/parse_replication_stream_test.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import mock
 import pytest
+import signal
+import sys
 
 from pymysqlreplication.event import QueryEvent
 from pymysqlreplication.row_event import WriteRowsEvent
@@ -9,6 +11,11 @@ from replication_handler.batch.parse_replication_stream import ParseReplicationS
 from replication_handler.components.binlogevent_yielder import ReplicationHandlerEvent
 from replication_handler.components.data_event_handler import DataEventHandler
 from replication_handler.components.schema_event_handler import SchemaEventHandler
+from replication_handler.components.stubs.stub_dp_clientlib import DPClientlib
+from replication_handler.models.database import rbr_state_session
+from replication_handler.models.data_event_checkpoint import DataEventCheckpoint
+from replication_handler.models.global_event_state import GlobalEventState
+from replication_handler.models.global_event_state import EventType
 
 
 class TestParseReplicationStream(object):
@@ -47,6 +54,67 @@ class TestParseReplicationStream(object):
             'handle_event',
         ) as mock_handle_event:
             yield mock_handle_event
+
+    @pytest.yield_fixture
+    def patch_get_latest_published_offset(self):
+        with mock.patch.object(
+            DPClientlib,
+            'get_latest_published_offset'
+        ) as mock_get_latest_published_offset:
+            yield mock_get_latest_published_offset
+
+    @pytest.yield_fixture
+    def patch_flush(self):
+        with mock.patch.object(
+            DPClientlib,
+            'flush'
+        ) as mock_get_latest_published_offset:
+            yield mock_get_latest_published_offset
+
+    @pytest.yield_fixture
+    def patch_upsert_global_event_state(self):
+        with mock.patch.object(
+            GlobalEventState, 'upsert'
+        ) as mock_upsert_global_event_state:
+            yield mock_upsert_global_event_state
+
+    @pytest.yield_fixture
+    def patch_create_data_event_checkpoint(self):
+        with mock.patch.object(
+            DataEventCheckpoint,
+            'create_data_event_checkpoint'
+        ) as mock_create_data_event_checkpoint:
+            yield mock_create_data_event_checkpoint
+
+    @pytest.yield_fixture
+    def patch_rbr_state_rw(self, mock_rbr_state_session):
+        with mock.patch.object(
+            rbr_state_session,
+            'connect_begin'
+        ) as mock_session_connect_begin:
+            mock_session_connect_begin.return_value.__enter__.return_value = \
+                mock_rbr_state_session
+            yield mock_session_connect_begin
+
+    @pytest.yield_fixture
+    def patch_exit(self):
+        with mock.patch.object(
+            sys,
+            'exit'
+        ) as mock_exit:
+            yield mock_exit
+
+    @pytest.yield_fixture
+    def patch_signal(self):
+        with mock.patch.object(
+            signal,
+            'signal'
+        ) as mock_signal:
+            yield mock_signal
+
+    @pytest.fixture
+    def mock_rbr_state_session(self):
+        return mock.Mock()
 
     def test_replication_stream_different_events(
         self,
@@ -102,3 +170,48 @@ class TestParseReplicationStream(object):
             mock.call(data_event, 'fake_gtid_2')
         ]
         assert patch_data_handle_event.call_count == 2
+
+    def test_handle_graceful_termination_data_event(
+        self,
+        patch_binlog_yielder,
+        patch_data_handle_event,
+        patch_rbr_state_rw,
+        patch_get_latest_published_offset,
+        patch_flush,
+        patch_create_data_event_checkpoint,
+        patch_upsert_global_event_state,
+        patch_exit,
+        patch_signal
+    ):
+        replication_stream = ParseReplicationStream()
+        assert patch_signal.call_count == 2
+        replication_stream.current_event_type = EventType.DATA_EVENT
+        replication_stream._handle_graceful_termination(mock.Mock(), mock.Mock())
+        assert patch_get_latest_published_offset.call_count == 1
+        assert patch_create_data_event_checkpoint.call_count == 1
+        assert patch_flush.call_count == 1
+        assert patch_upsert_global_event_state.call_count == 1
+        assert patch_exit.call_count == 1
+
+    def test_handle_graceful_termination_schema_event(
+        self,
+        patch_binlog_yielder,
+        patch_data_handle_event,
+        patch_rbr_state_rw,
+        patch_get_latest_published_offset,
+        patch_flush,
+        patch_create_data_event_checkpoint,
+        patch_upsert_global_event_state,
+        patch_exit,
+        patch_signal
+    ):
+        replication_stream = ParseReplicationStream()
+        assert patch_signal.call_count == 2
+        replication_stream.current_event_type = EventType.SCHEMA_EVENT
+        replication_stream._handle_graceful_termination(mock.Mock(), mock.Mock())
+        assert patch_get_latest_published_offset.call_count == 0
+        assert patch_create_data_event_checkpoint.call_count == 0
+        assert patch_flush.call_count == 0
+        assert patch_upsert_global_event_state.call_count == 0
+        assert patch_exit.call_count == 1
+        assert patch_signal.call_count == 2
