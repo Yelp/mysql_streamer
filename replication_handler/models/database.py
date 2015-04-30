f9878aa3357382dc6371c201903a127212406e22
Cheng Chen <cheng@yelp.com>
2015-04-30 16:37:33 -0700

    modify comments

--- replication_handler/models/database.py
+++ replication_handler/models/database.py
@@ -28,7 +28,7 @@ rbr_state_session = scoped_session(
 
 
 class UnixTimeStampType(types.TypeDecorator):
-    """A datetime.datetime that is stored as a unix timestamp."""
+    """ A datetime.datetime that is stored as a unix timestamp."""
     impl = types.Integer
 
     def process_bind_param(self, value, dialect=None):
@@ -47,32 +47,21 @@ def default_now(context):
 
 
 class JSONType(types.TypeDecorator):
-
-    """
-    A JSONType is stored in the db as a string and we interact with it like a
-    dict
-
-    Note that special objects (e.g., ObjectId, Currency, datetime) will be
-    str'd before dumping into the db, so some work will need to be done to
-    recover those objects when they are loaded. See JSONSerializedType if
-    you'd like to preserve these objects on loading.
+    """ A JSONType is stored in the db as a string and we interact with it like a
+    dict.
     """
     impl = types.Text
     separators = (',', ':')
 
     def process_bind_param(self, value, dialect=None):
-        """
-        Dump our value to a form our db recognizes (a string).
-        """
+        """ Dump our value to a form our db recognizes (a string)."""
         if value is None:
             return None
 
         return json.dumps(value, separators=self.separators)
 
     def process_result_value(self, value, dialect=None):
-        """
-        Convert what we get from the db into a json dict
-        """
+        """ Convert what we get from the db into a json dict"""
         if value is None:
             return None
         return json.loads(value)
