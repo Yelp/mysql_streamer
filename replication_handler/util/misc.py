a0dcf69f922453da93568aba0bf1f80b8cfc9d27
Cheng Chen <cheng@yelp.com>
2015-08-10 18:13:36 -0700

    add replication producer name and change message type

--- replication_handler/util/misc.py
+++ replication_handler/util/misc.py
@@ -5,6 +5,9 @@ from replication_handler.models.global_event_state import EventType
 from replication_handler.models.global_event_state import GlobalEventState
 
 
+REPLICATION_HANDLER_PRODUCER_NAME = "replication_handler"
+
+
 class ReplicationHandlerEvent(object):
     """ Class to associate an event and its position."""
 
@@ -18,13 +21,13 @@ class DataEvent(object):
     row per event.
     """
 
-    def __init__(self, schema, table, log_pos, log_file, row, event_type):
+    def __init__(self, schema, table, log_pos, log_file, row, message_type):
         self.schema = schema
         self.table = table
         self.log_pos = log_pos
         self.log_file = log_file
         self.row = row
-        self.event_type = event_type
+        self.message_type = message_type
 
 
 def save_position(position_data, is_clean_shutdown=False):
