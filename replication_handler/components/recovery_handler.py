1b37c7203907fd8bac829bdebb24ee47ef5f6b3f
Justin Cunningham <justinc@yelp.com>
2015-10-02 16:20:23 -0700

    Will use appropriate db before executing SQL.

--- replication_handler/components/recovery_handler.py
+++ replication_handler/components/recovery_handler.py
@@ -6,25 +6,19 @@ import logging
 
 import simplejson as json
 from pymysqlreplication.event import QueryEvent
-from yelp_conn.connection_set import ConnectionSet
 
+from replication_handler.components._pending_schema_event_recovery_handler import PendingSchemaEventRecoveryHandler
 from replication_handler.components.base_event_handler import Table
 from replication_handler.components.schema_event_handler import should_filter_query_event
 from replication_handler.config import source_database_config
 from replication_handler.models.data_event_checkpoint import DataEventCheckpoint
 from replication_handler.models.database import rbr_state_session
-from replication_handler.models.schema_event_state import SchemaEventState
-from replication_handler.models.schema_event_state import SchemaEventStatus
 from replication_handler.util.message_builder import MessageBuilder
 from replication_handler.util.misc import DataEvent
 from replication_handler.util.misc import save_position
 
 
 log = logging.getLogger('replication_handler.components.recvoery_handler')
-
-
-class BadSchemaEventStateException(Exception):
-    pass
 
 
 class RecoveryHandler(object):
@@ -82,11 +76,7 @@ class RecoveryHandler(object):
     def _handle_pending_schema_event(self):
         if self.pending_schema_event:
             log.info("Recovering from pending schema event: %s" % repr(self.pending_schema_event))
-            self._assert_event_state_status(
-                self.pending_schema_event,
-                SchemaEventStatus.PENDING
-            )
-            self._rollback_pending_event(self.pending_schema_event)
+            PendingSchemaEventRecoveryHandler(self.pending_schema_event).recover()
 
     def _handle_unclean_shutdown(self):
         if not self.is_clean_shutdown and isinstance(self.stream.peek().event, DataEvent):
@@ -112,53 +102,6 @@ class RecoveryHandler(object):
             self.producer.ensure_messages_published(messages, topic_offsets)
             position_data = self.producer.get_checkpoint_position_data()
             save_position(position_data)
-
-    def _assert_event_state_status(self, event_state, status):
-        if event_state.status != status:
-            log.error("schema_event_state has bad state, \
-                id: {0}, status: {1}, table_name: {2}".format(
-                event_state.id,
-                event_state.status,
-                event_state.table_name
-            ))
-            raise BadSchemaEventStateException
-
-    def _rollback_pending_event(self, pending_event_state):
-        # if pending statement is alter table statement, then we need to recreate the table.
-        # if pending statement is create table statement, just remove that table.
-        if pending_event_state.query.lower().startswith("create table"):
-            self._drop_table(pending_event_state.table_name)
-        else:
-            self._recreate_table(
-                pending_event_state.table_name,
-                pending_event_state.create_table_statement,
-            )
-        with rbr_state_session.connect_begin(ro=False) as session:
-            log.info("Removing schema event: %s" % pending_event_state.id)
-            SchemaEventState.delete_schema_event_state_by_id(session, pending_event_state.id)
-            session.commit()
-
-    def _drop_table(self, table_name):
-        log.info("Dropping table: %s" % table_name)
-        cursor = ConnectionSet.schema_tracker_rw().repltracker.cursor()
-        drop_table_query = "DROP TABLE `{0}`".format(
-            table_name
-        )
-        cursor.execute(drop_table_query)
-
-    def _create_table(self, create_table_statement):
-        log.info("Creating table: %s" % create_table_statement)
-        cursor = ConnectionSet.schema_tracker_rw().repltracker.cursor()
-        cursor.execute(create_table_statement)
-
-    def _recreate_table(self, table_name, create_table_statement):
-        """Restores the table with its previous create table statement,
-        because MySQL implicitly commits DDL changes, so there's no transactional
-        DDL. see http://dev.mysql.com/doc/refman/5.5/en/implicit-commit.html for more
-        background.
-        """
-        self._drop_table(table_name)
-        self._create_table(create_table_statement)
 
     def _build_messages(self, events):
         messages = []
