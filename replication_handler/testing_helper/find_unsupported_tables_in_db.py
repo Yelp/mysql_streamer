81ec024fdeb30b0f35189738702b6dcbdb7b9512
Abrar Sheikh <abrar@yelp.com>
2016-04-20 16:10:30 -0700

    script to find unsupported table in DB

--- replication_handler/testing_helper/find_unsupported_tables_in_db.py
+++ replication_handler/testing_helper/find_unsupported_tables_in_db.py
@@ -29,20 +29,21 @@ def execute_query_get_all_rows(db_name, query):
     finally:
         connection.close()
 
-unspported_types = set(['BIT', 'DECIMAL', 'DATE', 'DATETIME', 'TIMESTAMP',
-                        'TIME', 'ENUM', 'SET'])
+if __name__ == "__main__":
+    unspported_types = set(['BIT', 'DECIMAL', 'DATE', 'DATETIME', 'TIMESTAMP',
+                            'TIME', 'ENUM', 'SET'])
 
-query = "SELECT * FROM information_schema.columns WHERE table_schema = 'yelp';"
-table_columns = execute_query_get_all_rows('yelp', query)
+    query = "SELECT * FROM information_schema.columns WHERE table_schema = 'yelp';"
+    table_columns = execute_query_get_all_rows('yelp', query)
 
-table_to_types_map = defaultdict(set)
+    table_to_types_map = defaultdict(set)
 
-for table_column in table_columns:
-    table_to_types_map[table_column['TABLE_NAME']].add(
-        table_column['DATA_TYPE'].upper()
-    )
+    for table_column in table_columns:
+        table_to_types_map[table_column['TABLE_NAME']].add(
+            table_column['DATA_TYPE'].upper()
+        )
 
-for table_name in table_to_types_map:
-    inter = table_to_types_map[table_name].intersection(unspported_types)
-    if inter:
-        print "{0} : {1}".format(table_name, inter)
+    for table_name in table_to_types_map:
+        inter = table_to_types_map[table_name].intersection(unspported_types)
+        if inter:
+            print "{0} : {1}".format(table_name, inter)
