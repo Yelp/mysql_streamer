c61d0acfaaf748c3cc2de3a1446b11d0c48ad6e6
Justin Cunningham <justinc@yelp.com>
2016-04-21 19:07:03 -0700

    Refactored upsert_data_event_checkpoint to only execute a maximum of 3 SQL statements.

--- replication_handler/models/data_event_checkpoint.py
+++ replication_handler/models/data_event_checkpoint.py
@@ -6,10 +6,10 @@ import logging
 import time
 
 from data_pipeline.tools.meteorite_wrappers import StatTimer
+from sqlalchemy import bindparam
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import String
-from yelp_lib.containers.lists import unlist
 
 from replication_handler import config
 from replication_handler.models.database import Base
@@ -48,27 +48,67 @@ class DataEventCheckpoint(Base):
             rbr_source_cluster=config.env_config.rbr_source_cluster,
         )
         timer.start()
+
+        existing_topics_to_records = cls._get_topic_to_checkpoint_record_map(
+            session,
+            cluster_name
+        )
+        new_checkpoints = []
+        updated_checkpoints = []
         for topic, offset in topic_to_kafka_offset_map.iteritems():
-            data_event_checkpoint = session.query(
-                DataEventCheckpoint
-            ).filter(
-                DataEventCheckpoint.kafka_topic == topic,
-                DataEventCheckpoint.cluster_name == cluster_name
-            ).all()
-            data_event_checkpoint = unlist(data_event_checkpoint)
-            if data_event_checkpoint is None:
-                data_event_checkpoint = DataEventCheckpoint()
-            data_event_checkpoint.kafka_topic = topic
-            data_event_checkpoint.kafka_offset = offset
-            data_event_checkpoint.cluster_name = cluster_name
+            if topic in existing_topics_to_records:
+                existing_record = existing_topics_to_records[topic]
+                if existing_record.kafka_offset != offset:
+                    updated_checkpoints.append({
+                        'checkpoint_id': existing_record.id,
+                        'kafka_offset': offset
+                    })
+            else:
+                new_checkpoints.append({
+                    'kafka_topic': topic,
+                    'kafka_offset': offset,
+                    'cluster_name': cluster_name
+                })
             # Log data with current time (not necessarily
             # the time on the event time field)
-            log.info(
+            log.debug(
                 'Reached checkpoint with offset {} on topic {} at time {}.'.
                 format(offset, topic, int(time.time()))
             )
-            session.add(data_event_checkpoint)
+
+        # TODO(justinc|DATAPIPE-920) Switch to the SQLAlchemy bulk save
+        # API, when it's available.  If you pick that up, you'll probably
+        # want to base the code off of commit
+        # c0632aacbfb54bfba8b35285dc5dd791b35f16a5 instead of this, since it'll
+        # be closer.
+        table = cls.__table__
+        if new_checkpoints:
+            session.execute(table.insert(), new_checkpoints)
+        if updated_checkpoints:
+            session.execute(
+                table.update().where(
+                    # Using checkpoint_id instead of id since id is reserved
+                    # by SQLA
+                    table.c.id == bindparam('checkpoint_id')
+                ).values(
+                    kafka_offset=bindparam('kafka_offset')
+                ),
+                updated_checkpoints
+            )
+
         timer.stop()
+
+    @classmethod
+    def _get_topic_to_checkpoint_record_map(cls, session, cluster_name):
+        records = session.query(
+            DataEventCheckpoint
+        ).filter(
+            DataEventCheckpoint.cluster_name == cluster_name
+        ).all()
+        topic_to_checkpoint_record_map = {}
+        for record in records:
+            topic_to_checkpoint_record_map[record.kafka_topic] = record
+        return topic_to_checkpoint_record_map
 
     @classmethod
     def get_topic_to_kafka_offset_map(cls, session, cluster_name):
