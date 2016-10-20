f2ce7cb5c148a5a38eb4ae60954815bc7e47dd46
Abrar Sheikh <abrar@yelp.com>
2016-10-19 18:22:39 -0700

    fixed an issue with parse replication stream test

--- tests/batch/parse_replication_stream_test.py
+++ tests/batch/parse_replication_stream_test.py
@@ -6,7 +6,7 @@ from replication_handler.batch.parse_replication_stream import ParseReplicationS
 from tests.batch.base_parse_replication_stream_test import BaseParseReplicationStreamTest
 
 
-class BaseParseReplicationStreamTest(BaseParseReplicationStreamTest):
+class TestParseReplicationStream(BaseParseReplicationStreamTest):
 
     def _get_parse_replication_stream(self):
         return ParseReplicationStream()
